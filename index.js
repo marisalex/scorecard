63bbef46203358d799a3f0347676682b62b47368
marisalex <101093957+marisalex@users.noreply.github.com>
2022-12-14 21:52:40 +0100

    commit to main

---
+++ index.js
@@ -0,0 +1,35 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+score = 0
+
+// Home Score//
+function home1() {
+    score += 1
+    homeScore.textContent = score
+}
+
+function home2() {
+    score += 2
+    homeScore.textContent = score
+}
+
+function home3() {
+    score += 3
+    homeScore.textContent = score
+}
+
+//Guest Score//
+function guest1() {
+    score += 1
+    guestScore.textContent = score
+}
+
+function guest2() {
+    score += 2
+    guestScore.textContent = score
+}
+
+function guest3() {
+    score += 3
+    guestScore.textContent = score
+}
